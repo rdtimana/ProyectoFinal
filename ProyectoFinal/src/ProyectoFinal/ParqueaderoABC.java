d98b3f2f215e8ab4611395933e0e97a5e3fe7ef6
rdtimana <66284972+rdtimana@users.noreply.github.com>
2020-07-04 20:04:45 -0500

    Menu

--- ProyectoFinal/src/ProyectoFinal/ParqueaderoABC.java
+++ ProyectoFinal/src/ProyectoFinal/ParqueaderoABC.java
@@ -1,10 +1,26 @@
 package ProyectoFinal;
 
+import java.util.Scanner;
+
 public class ParqueaderoABC {
-
+	
+	static Scanner teclado = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-
-		System.out.println("Inicio");
+	
+		System.out.println("Seleccione la opción deseada");
+		System.out.println("(1) Registrar Entrada");
+		System.out.println("(2) Registrar Salida");
+		System.out.println("(2) Registrar Salida");
+		int opcion = teclado.nextInt();
+		switch (opcion) {
+		case 1:
+			System.out.println("Inicia");
+			break;
+		case 2:
+			System.out.println("Salio");
+			break;
+		}
 	}
 
 }
